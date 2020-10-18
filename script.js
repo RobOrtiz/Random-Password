ca1901e68c837582ffd4820178bbaa1e2384f829
RobOrtiz <mrrobortiz@gmail.com>
2020-10-17 23:26:36 -0700

    fixing js file

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
-var passLength = "";
+
 var passSpecial;
 var passNumeric;
 var passUpper;
 var passLower;
 
 // Character Arrays 
-var charnumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var charspecial = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var charNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var charSpecial = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var charLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector(".btn");
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+generateBtn.addEventListener("click", generatePassword);
 
 // Prompt to confirm how many characters the user would like in their password
 function generatePassword() {
-  prompt("How many characters would you like your password to contain?");
-   
-  if (passLength > 8 || passLength < 128) {
-    alert(`Your password will have ${passLength} characters`);
-  } else alert("Password length must be between 8-128 characters. Try again");
-};   
-    
+  var passLength = prompt("How many characters would you like your password to contain?");
 
-// Confirm character types
-passLower = confirm("Would you like to use lowercase letters?");
-passUpper = confirm("Would you like to use uppercase letter?");
-passSpecial = confirm("Would you like to use special characters?");
-passNumeric = confirm("Would you like to use numeric characters?");    
-
-while (passUpper === false && passLower === false && passSpecial === false && passNumeric === false) {
-    alert("You must choose at least one parameter");
-    passLower = confirm("Would you like to use lowercase letters?");
-    passUpper = confirm("Would you like to use uppercase letter?");
-    passSpecial = confirm("Would you like to use special characters?");
-    passNumeric = confirm("Would you like to use numeric characters?");  
+if (passLength > 8 || passLength < 128){
+  confirm("Your password will be " + passLength + " charcters long");
 } 
- 
-var passwordCharacters = []
+if (passLength < 8 || passLength > 128){
+  alert("Length must be 8-128 characters");
+  return;
+} 
+var passLower = confirm("Would you like to use lowercase letters?");
+var passUpper = confirm("Would you like to use uppercase letter?");
+var passSpecial = confirm("Would you like to use special characters?");
+var passNumeric = confirm("Would you like to use numeric characters?");  
+if (passUpper === false && passLower === false && passSpecial === false && passNumeric === false) {
+  alert("You must choose at least one parameter");
+  passLower = confirm("Would you like to use lowercase letters?");
+  passUpper = confirm("Would you like to use uppercase letter?");
+  passSpecial = confirm("Would you like to use special characters?");
+  passNumeric = confirm("Would you like to use numeric characters?");  
+} 
 
+var passwordCharacters = []
 if (passLower) {
   passwordCharacters = passwordCharacters.concat(charLower)
 }
@@ -60,14 +57,15 @@ if (passNumeric) {
   
 console.log(passwordCharacters);
 
+// Randomly select characters according to length
 var randomPassword = "";
       
-  for (var i = 0; i < confirmLength; i++) {
+  for (var i = 0; i < passLength; i++) {
     randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
     console.log(randomPassword)
-  }
-  return randomPassword;
+  };
 
+};
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
